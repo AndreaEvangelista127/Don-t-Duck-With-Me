57ee06f183765e8b2fa8e901e8ec2637f24bd96c
GOOROOM02 <abchohyun02@naver.com>
2026-01-17 17:23:57 +0100

    quack

--- Assets/Scripts/Chohyun/MainMenuManager.cs
+++ Assets/Scripts/Chohyun/MainMenuManager.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,17 +7,33 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private GameObject MainPanel;
     [SerializeField] private GameObject CreditsPanel;
 
+    [SerializeField] private AudioSource UIAudioSource;
+    [SerializeField] private AudioClip Quack;
+
+
+    public void Start()
+    {
+        if (CreditsPanel != null)
+        {
+            CreditsPanel.SetActive(false);
+            MainPanel.SetActive(true);
+        }
+    }
     public void StartGame() 
     {
+        PlayClick();
+        float delay = (Quack != null) ? Quack.length : 0.5f;
         SceneManager.LoadScene("Playground");
     }
     public void QuitGame() 
     {
+        
         Application.Quit();
     }
 
     public void OpenCredits() 
     {
+        PlayClick();
         if (CreditsPanel != null) 
         { CreditsPanel.SetActive(true);
             MainPanel.SetActive(false);
@@ -26,11 +43,20 @@ public class MainMenuController : MonoBehaviour
 
     public void CloseCredits() 
     {
+        PlayClick();
         if (CreditsPanel != null)
         { CreditsPanel.SetActive(false);
             MainPanel.SetActive(true);
         }
     }
+
+    void PlayClick() 
+    {
+        if (UIAudioSource != null && Quack != null) { UIAudioSource.PlayOneShot(Quack); }
+    }
+  
+        
+   
 
 
 }
