80fbe5dd4a91dce42815150c843ea4fdc197fd46
l1ko07 <danielcharisma07@gmail.com>
2026-01-17 16:40:22 +0100

    Particel fertig

---
+++ Assets/Scripts/Daniel/CrossItem.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CrossItem : BasePickUp
+{
+    [SerializeField] private int _playerHealth;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D(collision);
+    }
+
+    public override void PickUp()
+    {
+
+    }
+}
